c769db70b0d5131b08d04bd0c0c97d192f01e663
Diovane Alves de Oliveira <alvdiovane@gmail.com>
2022-07-18 21:54:03 -0300

    smartphone bug fixing
    Alignment to the center of the screen on mobile devices

--- src/script.js
+++ src/script.js
@@ -1,11 +1,11 @@
 const btn = document.querySelector('.btn')
-const container = document.querySelector('.container')
+const body = document.querySelector('.body')
 const logo = document.querySelector('.logo')
 const link = document.querySelectorAll('.list li')
 
 btn.onclick = function(){
     this.classList.toggle('active')
-    container.classList.toggle('active')
+    body.classList.toggle('active')
     logo.classList.toggle('active')
 
     var link_array= [...link]
